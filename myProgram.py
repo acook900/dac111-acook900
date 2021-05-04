3208395c8032f92a1f5d1d43bece02e24b5f57aa
ashleycook <acook900@uwsp.edu>
2021-05-04 10:45:49 -0500

    Added the main function and the main function call

--- myProgram.py
+++ myProgram.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue May  4 10:12:46 2021
-
+Created on Tue May  3 10:12:46 2021
 @author: ashleycook
 """
 
+def main():
+    print('Hello World')
+    
+main()
