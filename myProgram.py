7e122b838d97eb0e179d4d65eec49932c50ee8ba
ashleycook <acook900@uwsp.edu>
2021-05-04 11:56:38 -0500

    Added code to get the top 5 rows from the flights.db

--- myProgram.py
+++ myProgram.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue May  3 10:12:46 2021
+Created on Tue May 4 10:12:46 2021
 @author: ashleycook
 """
 
+import sqlite3 as sl
+
+FLIGHTS_DB = "/Users/ashleycook/Downloads/flights.db"
+
 def main():
-    print('Hello World')
+    #Holds connection between database so we can interact with database
+    connection = sl.connect(FLIGHTS_DB)
+    cursor = connection.cursor()
+    
+    query = "select * from airlines limit 5;"
+    cursor.execute(query)
+    
+    results = cursor.fetchall()
+    print(results)
+    
+    cursor.close()
+    connection.close()
     
 main()
