b340771ec9341581d2f537c0815581e819f5ba21
ashleycook <acook900@uwsp.edu>
2021-05-04 12:29:56 -0500

    Added the mapping for the world projection

--- myProgram.py
+++ myProgram.py
@@ -4,8 +4,12 @@
 Created on Tue May 4 10:12:46 2021
 @author: ashleycook
 """
+import os 
 
+os.environ["PROJ_LIB"] = "/Applications/Anaconda-Navigator.app"
 import sqlite3 as sl
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
 
 FLIGHTS_DB = "/Users/ashleycook/Downloads/flights.db"
 
@@ -14,13 +18,30 @@ def main():
     connection = sl.connect(FLIGHTS_DB)
     cursor = connection.cursor()
     
-    query = "select * from airlines limit 5;"
-    cursor.execute(query)
     
-    results = cursor.fetchall()
-    print(results)
+    coords = cursor.execute(
+"""
+select cast(longitude as float), 
+cast(latitude as float)
+from airports;                        
+"""
+    ).fetchall()
     
     cursor.close()
     connection.close()
+
+    myMap = Basemap(
+        projection = 'merc',
+        llcrnrlat = -80,
+        urcrnrlat = 80, 
+        llcrnrlon = -180,
+        urcrnrlon = 180,
+        lat_ts = 20,
+        resolution = 'c')
+    
+    myMap.drawcoastlines()
+    myMap.drawmapboundary()
+    
+
     
 main()
